94cc56762fb46a0cc25f4f2f662e776a2c57691c
munishsagar <sagarvirk1@gmail.com>
2023-08-08 15:10:39 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./mongoDb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+
+import dalleRoutes from "./routes/dalleRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
+const startServer = async () => {
+  try {
+    connectDb(process.env.MONGO_URL);
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
